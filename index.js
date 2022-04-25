10af21fc3ad89b966c298f4603a94ca6ac152e08
Casper <casper.aakviststeengaard@gmail.com>
2022-04-25 10:42:25 +0200

    Added pageable

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { renderTemplate, setActive, showPage } from "./utils.js"
+import { setupHandeler } from "./js-for-pages/page1.js"
+import { showText } from "./js-for-pages/page2.js"
+import { load} from "./js-for-pages/page3.js"
 
 
 
@@ -12,12 +15,15 @@ function renderMenuItems(evt) {
   switch (id) {
     //Here you can execute JavaScript for the selected page
     case "page-1": {
+      setupHandeler
       break
     }
     case "page-2": {
+      showText
       break
     }
     case "page-3": {
+      load
       break
     }
   }
--- js-for-pages/page2.js
+++ js-for-pages/page2.js
@@ -1,2 +1,8 @@
+import { text, setText} from "./page1.js"
 
 
+export function showText(){
+    //Change text to something else
+    setText("This has been changed")
+    document.getElementById("text").innerText = text
+}
